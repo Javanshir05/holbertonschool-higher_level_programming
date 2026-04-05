be1d4ef6b1d0626573c133736f7c491951bb4640
root <jhashimov17871@ada.edu.az>
2026-04-05 17:14:05 +0000

    Don

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,10 +1,35 @@
 #!/usr/bin/python3
+"""
+This module defines a Square class.
+It features private attribute validation and a public area method.
+"""
+
+
 class Square:
+    """
+    Defines a square by its size.
+
+    Attributes:
+        __size (int): Private size of the square.
+    """
     def __init__(self, size=0):
+        """
+        Initializes a new Square instance.
+
+        Args:
+            size (int): The size of the square.
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
     def area(self):
-        return (self.__size * self.__size)
+        """
+        Computes the current square area.
+
+        Returns:
+            int: The area of the square.
+        """
+        return self.__size * self.__size
