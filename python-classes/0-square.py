2bf394d0bd2e967a0a8661cee756150cdf21a870
root <jhashimov17871@ada.edu.az>
2026-04-05 17:08:32 +0000

    Done

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
+"""
+This module defines a simple Square class.
+It is a practice exercise for object-oriented programming.
+"""
+
+
 class Square:
+    """
+    An empty class that defines a square.
+    """
     pass
